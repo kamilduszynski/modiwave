3c8e6d7643281f5b4b4b03bf6e0c831f459cc050
kamil <camilos.duszynski@gmail.com>
2023-06-01 22:07:44 +0200

    upgrade plotting

--- silence_cutter.py
+++ silence_cutter.py
@@ -9,34 +9,41 @@ from scipy.io import wavfile
 
 ## Local imports
 
+def separate_silence_and_noise(channel, threshold=0.0001):
+    silence_parts = np.where(np.abs(channel)<threshold)[0]
+    noise_parts = np.where(np.abs(channel)>threshold)[0]
+    silence_sample_points = channel[silence_parts].shape[0]
+    noise_sample_points = channel[noise_parts].shape[0]
+    return noise_parts, noise_sample_points, silence_parts, silence_sample_points
+
 def calculate_time_array(sample_points, sampling_freq):
     time_array = np.arange(0, sample_points, 1)
     time_array = time_array / sampling_freq
     time_array = time_array / 60
     return time_array
 
-def plot_amplitude(time_arrays, channels_1, channels_2):
-    
-    #Plot the tone
-    for ind, timeArray in enumerate(time_arrays):
-        fig, axs = plt.subplots(2, 1)
-        fig.suptitle('Audio amplitude', fontsize=16)
-        fig.set_figheight(7)
-        fig.set_figwidth(12)
-        axs[0].plot(timeArray, channels_1[ind], color='r')
-        axs[0].set_title('channel 1')
-        axs[0].set_xlabel('Time (min)')
-        axs[0].set_ylabel('Amplitude')
-        axs[1].plot(timeArray, channels_2[ind], color='g')
-        axs[1].set_title('channel 2')
-        axs[1].set_xlabel('Time (min)')
-        axs[1].set_ylabel('Amplitude')
+def plot_amplitude(time_arrays, sound_arrays):
+    # Plot orginal audio, silence and noise parts amplitudes
+    fig, axs = plt.subplots(3, 1)
+    fig.suptitle('Audio amplitude', fontsize=16)
+    fig.set_figheight(10)
+    fig.set_figwidth(16)
+    axs[0].plot(time_arrays[0], sound_arrays[0], color='r')
+    axs[0].set_title('Original audio')
+    axs[0].set_ylabel('Amplitude')
+    axs[1].plot(time_arrays[1], sound_arrays[1], color='g')
+    axs[1].set_title('silence parts')
+    axs[1].set_ylabel('Amplitude')
+    axs[2].plot(time_arrays[2], sound_arrays[2], color='b')
+    axs[2].set_title('noise parts')
+    axs[2].set_xlabel('Duration (min)')
+    axs[2].set_ylabel('Amplitude')
     plt.show()
 
 def main() -> int:
     myAudio = "audio.wav"
 
-    # Read file and get sampling freq [ usually 44100 Hz ]  and sound object
+    # Read file and get sampling frequency and sound object
     sampling_freq, sound = wavfile.read(myAudio)
 
     # Check if wave file is 16bit or 32 bit. 24bit is not supported
@@ -44,55 +51,54 @@ def main() -> int:
 
     # We can convert our sound array to floating point values ranging from -1 to 1 as follows
     sound = sound / (2.**15)
+    max_amplitude = np.abs(sound).max()
+    min_amplitude = np.abs(sound).min()
 
-    #Check sample points and sound channel for duel channel(5060, 2) or  (5060, ) for mono channel
+    # Perform checking only on one channel in case audio is mono
     sound_shape = sound.shape
-    channel_1 = sound[:,0]
-    channel_2 = sound[:,1]
+    channel_0 = sound[:,0]
+
+    if sound.shape[1] == 2:
+        channel_1 = sound[:,0]
 
     threshold = 0.0001
-    max = np.abs(sound).max()
-    min = np.abs(sound).min()
-    silent_parts = np.where(np.abs(channel_1)<threshold)[0]
-    noisy_parts = np.where(np.abs(channel_1)>threshold)[0]
-
     sample_points = sound.shape[0]
-    silent_sample_points = sound[silent_parts].shape[0]
-    noisy_sample_points = sound[noisy_parts].shape[0]
+    noise_parts, noise_sample_points, silence_parts, silence_sample_points = separate_silence_and_noise(channel_0, threshold)
 
     time_array = calculate_time_array(sample_points, sampling_freq)
-    silent_time_array = calculate_time_array(silent_sample_points, sampling_freq)
-    noisy_time_array = calculate_time_array(noisy_sample_points, sampling_freq)
+    silence_time_array = calculate_time_array(silence_sample_points, sampling_freq)
+    noise_time_array = calculate_time_array(noise_sample_points, sampling_freq)
 
-    audio_duration =  math.modf(np.round(sample_points / sampling_freq / 60, 2))
-    silent_audio_duration =  math.modf(np.round(silent_sample_points / sampling_freq / 60, 2))
-    noisy_audio_duration =  math.modf(np.round(noisy_sample_points / sampling_freq / 60, 2))
+    original_audio_duration =  math.modf(np.round(sample_points / sampling_freq / 60, 2))
+    silence_audio_duration =  math.modf(np.round(silence_sample_points / sampling_freq / 60, 2))
+    noise_audio_duration =  math.modf(np.round(noise_sample_points / sampling_freq / 60, 2))
     
+    time_arrays = [time_array, silence_time_array, noise_time_array]
+    sound_arrays = [sound, sound[silence_parts, :], sound[noise_parts, :]]
+
     print("#############################################")
     print(f"|sound data type:           {sound_data_type}")
     print(f"|sound data shape:          {sound_shape}")
     print(f"|")
-    print(f"|original samples:          {len(time_array)}")
-    print(f"|original audio duration:   {int(audio_duration[1])}min {int(60*audio_duration[0])}s")
+    print(f"|original samples:          {time_array.shape[0]}")
+    print(f"|original audio duration:   {int(original_audio_duration[1])}min {int(60*original_audio_duration[0])}s")
     print(f"|")
-    print(f"|silent samples:            {len(silent_time_array)}")
-    print(f"|silent audio duration:     {int(silent_audio_duration[1])}min {int(60*silent_audio_duration[0])}s")
+    print(f"|silence samples:           {silence_time_array.shape[0]}")
+    print(f"|silence audio duration:    {int(silence_audio_duration[1])}min {int(60*silence_audio_duration[0])}s")
     print(f"|")
-    print(f"|noisy samples:             {len(noisy_time_array)}")
-    print(f"|noisy audio duration:      {int(noisy_audio_duration[1])}min {int(60*noisy_audio_duration[0])}s")
+    print(f"|noise samples:             {noise_time_array.shape[0]}")
+    print(f"|noise audio duration:      {int(noise_audio_duration[1])}min {int(60*noise_audio_duration[0])}s")
     print(f"|")
     print(f"|sampling frequency:        {sampling_freq}")
-    print(f"|min amplitude:             {min}")
-    print(f"|max amplitude:             {max}")
+    print(f"|min amplitude:             {min_amplitude}")
+    print(f"|max amplitude:             {max_amplitude}")
     print("#############################################")
 
-    time_arrays = [time_array, silent_time_array, noisy_time_array]
-    channels_1 = [channel_1, channel_1[silent_parts], channel_1[noisy_parts]]
-    channels_2 = [channel_2, channel_2[silent_parts], channel_2[noisy_parts]]
+    wavfile.write("audio_silence.wav", sampling_freq, sound[silence_parts])
+    wavfile.write("audio_no_silence.wav", sampling_freq, sound[noise_parts])
 
-    # plot_amplitude(time_arrays, channels_1, channels_2)
-
-    wavfile.write("out.wav", sampling_freq, sound[noisy_parts])
+    if "-p" in sys.argv:
+        plot_amplitude(time_arrays, sound_arrays)
     return 0
 
 if __name__ == "__main__":
